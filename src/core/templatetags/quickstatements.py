f6d1b067751f4373734fb26d13acceaefcced922
Carybé <carybe.silva@usp.br>
2025-04-28 23:17:21 -0300

    Prevent action and status tags from breaking onto new lines
    Adds qualifiers and references to command list and adds labels to statements items
    Separates id validation and updates tests
    Removes redundant parsing and  rollsback to individual validation of entities
    Removes unecessary property methods

--- src/core/templatetags/quickstatements.py
+++ src/core/templatetags/quickstatements.py
@@ -8,3 +8,10 @@ register = template.Library()
 @register.simple_tag
 def has_multiple_wikibases():
     return Wikibase.objects.all().count() > 1
+
+
+@register.filter
+def get(dictionary, key):
+    if type(key) is not str or not type(dictionary) is dict:
+        return ""
+    return dictionary.get(key, "")
