e1e503662720a2714063c68bb7b8d376cef3a08e
arcstur <arcstur@gmail.com>
2025-05-02 10:19:06 -0300

    refactor: update statistics and add another test case

--- src/web/tests/test_statistics.py
+++ src/web/tests/test_statistics.py
@@ -1,17 +1,64 @@
+import requests_mock
 from django.contrib.auth.models import User
 from django.test import TestCase
 
+from core.factories import TokenFactory, UserFactory
+from core.parsers.v1 import V1CommandParser
+from core.tests.test_api import ApiMocker
+
 
 class ProfileTest(TestCase):
-    def test_view_profile(self):
+    def assertInRes(self, substring, response):
+        """Checks if a substring is contained in response content"""
+        self.assertIn(substring.lower(), str(response.content).lower().strip())
+
+    def test_empty(self):
         user = User.objects.create_user(username="john")
         self.client.force_login(user)
         response = self.client.get("/statistics/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed("profile.html")
+        self.assertTemplateUsed(response, "statistics.html")
         self.assertEqual(response.context["batches_count"], 0)
         self.assertEqual(response.context["commands_count"], 0)
         self.assertEqual(response.context["average_commands_per_batch"], 0)
         self.assertEqual(response.context["items_created"], 0)
         self.assertEqual(response.context["editors"], 0)
         self.assertEqual(response.context["edits"], 0)
+
+    @requests_mock.Mocker()
+    def test_basic(self, mocker):
+        api_mocker = ApiMocker()
+        api_mocker.is_autoconfirmed(mocker)
+        api_mocker.wikidata_property_data_types(mocker)
+        api_mocker.property_data_type(mocker, "P1", "wikibase-item")
+        api_mocker.create_item(mocker, "Q123")
+        user = UserFactory(username="wiki")
+        user2 = UserFactory(username="ikiw")
+        TokenFactory(user=user)
+        TokenFactory(user=user2)
+        v1 = V1CommandParser()
+        batch = v1.parse("Test", "wiki", "CREATE||LAST|P1|Q1||LAST|P1|Q2")
+        batch.combine_commands = True
+        batch.wikibase = api_mocker.wikibase
+        batch.save_batch_and_preview_commands()
+        batch.run()
+        batch = v1.parse("Test", "wiki", "CREATE||LAST|P1|Q5")
+        batch.combine_commands = True
+        batch.wikibase = api_mocker.wikibase
+        batch.save_batch_and_preview_commands()
+        batch.run()
+        batch = v1.parse("Test", "ikiw", "CREATE||LAST|P1|Q1||LAST|P1|Q2")
+        batch.combine_commands = True
+        batch.wikibase = api_mocker.wikibase
+        batch.save_batch_and_preview_commands()
+        batch.run()
+        self.client.force_login(user)
+        response = self.client.get("/statistics/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "statistics.html")
+        self.assertEqual(response.context["batches_count"], 3)
+        self.assertEqual(response.context["commands_count"], 8)
+        self.assertEqual(response.context["average_commands_per_batch"], 3)
+        self.assertEqual(response.context["items_created"], 3)
+        self.assertEqual(response.context["editors"], 2)
+        self.assertEqual(response.context["edits"], 3)
--- src/web/views/statistics.py
+++ src/web/views/statistics.py
@@ -6,17 +6,19 @@ from core.models import BatchCommand
 
 def statistics(request):
     batches_count = Batch.objects.all().count()
+    editors = Batch.objects.values("user").distinct().count()
     commands_count = BatchCommand.objects.all().count()
     average_commands_per_batch = (
         round(commands_count / batches_count) if batches_count > 0 else 0
     )
+    # TODO: we can refactor a lot of this into a BatchCommand model manager
     items_created = BatchCommand.objects.filter(
         operation=BatchCommand.Operation.CREATE_ITEM,
         status=BatchCommand.STATUS_DONE,
     ).count()
-    editors = Batch.objects.values_list("user", flat=True).distinct().count()
     edits = (
         BatchCommand.objects.filter(status=BatchCommand.STATUS_DONE)
+        .exclude(response_id__isnull=True)
         .exclude(response_id="")
         .count()
     )
