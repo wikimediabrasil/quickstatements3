4811e31b87c1a2de4d97e7d777a26ec6302704d1
Carybé <carybe.silva@usp.br>
2025-05-09 20:56:50 -0300

    Adds summary to batch listing

--- src/web/views/batches.py
+++ src/web/views/batches.py
@@ -2,8 +2,9 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.urls import reverse
+from django.db.models import Count, Q
 
-from core.models import Batch
+from core.models import Batch, BatchCommand
 
 
 PAGE_SIZE = 25
