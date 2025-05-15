2108d7f37f49b13c2e13300d0aeb230d3566c94d
Carybé <carybe.silva@usp.br>
2025-05-15 03:50:55 -0300

    Improves summary query  performance by annotating only the page batches and using raw SQL

--- src/web/views/batches.py
+++ src/web/views/batches.py
@@ -2,9 +2,8 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.urls import reverse
-from django.db.models import Count, Q
 
-from core.models import Batch, BatchCommand
+from core.models import Batch
 
 
 PAGE_SIZE = 25
