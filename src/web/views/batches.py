e7c9c35af60e8c3cee8280b9c2e379aa866d97d1
arcstur <arcstur@gmail.com>
2025-07-08 15:03:51 -0300

    refactor: batches page code and template
    * use HTML escape codes for &lt; and &gt;
    * refactor both views into one function
    * fix typo

--- src/web/views/batches.py
+++ src/web/views/batches.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
@@ -6,6 +8,7 @@ from django.urls import reverse
 from core.models import Batch
 
 PAGE_SIZE = 25
+MAX_PAGE_SIZE = 500
 
 
 @require_http_methods(["GET", "HEAD"])
@@ -16,31 +19,49 @@ def home(request):
     return render(request, "index.html")
 
 
+def _last_batches(request, user: Optional[str]):
+    try:
+        page = int(request.GET.get("page", 1))
+        page_size = min(int(request.GET.get("page_size", PAGE_SIZE)), MAX_PAGE_SIZE)
+    except (TypeError, ValueError):
+        page = 1
+        page_size = PAGE_SIZE
+
+    batches = Batch.objects.all()
+
+    if user:
+        batches = batches.filter(user=user)
+        base_url = reverse("last_batches_by_user", args=[user])
+    else:
+        base_url = reverse("last_batches")
+
+    batches = (
+        batches.exclude(status=Batch.STATUS_PREVIEW)
+        .with_command_status_counts()
+        .select_related("wikibase")
+        .order_by("-modified")
+    )
+    paginator = Paginator(batches, page_size)
+
+    return render(
+        request,
+        "batches.html",
+        {
+            # we need to use `username` since `user` is always supplied by django templates
+            "username": user,
+            "page": paginator.page(page),
+            "base_url": base_url,
+            "page_size": page_size,
+        },
+    )
+
+
 @require_http_methods(["GET"])
 def last_batches(request):
     """
     List last PAGE_SIZE batches modified
     """
-    try:
-        page = int(request.GET.get("page", 1))
-        page_size = int(request.GET.get("page_size", PAGE_SIZE))
-    except (TypeError, ValueError):
-        page = 1
-        page_size = PAGE_SIZE
-
-    qs = (
-        Batch.objects.exclude(status=Batch.STATUS_PREVIEW)
-        .with_command_status_counts()
-        .order_by("-modified")
-    )
-
-    paginator = Paginator(qs, page_size)
-    base_url = reverse("last_batches")
-    return render(
-        request,
-        "batches.html",
-        {"page": paginator.page(page), "base_url": base_url, "page_size": page_size},
-    )
+    return _last_batches(request, None)
 
 
 @require_http_methods(["GET"])
@@ -48,28 +69,4 @@ def last_batches_by_user(request, user):
     """
     List last PAGE_SIZE batches modified created by user
     """
-    try:
-        page = int(request.GET.get("page", 1))
-        page_size = int(request.GET.get("page_size", PAGE_SIZE))
-    except (TypeError, ValueError):
-        page = 1
-        page_size = PAGE_SIZE
-
-    batches = (
-        Batch.objects.filter(user=user)
-        .exclude(status=Batch.STATUS_PREVIEW)
-        .with_command_status_counts()
-    )
-    paginator = Paginator(batches.order_by("-modified"), page_size)
-    base_url = reverse("last_batches_by_user", args=[user])
-    # we need to use `username` since `user` is always supplied by django templates
-    return render(
-        request,
-        "batches.html",
-        {
-            "username": user,
-            "page": paginator.page(page),
-            "base_url": base_url,
-            "page_size": page_size,
-        },
-    )
+    return _last_batches(request, user)
